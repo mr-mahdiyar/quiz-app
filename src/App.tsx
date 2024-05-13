937ee8089db6a437ec2f18719cba9b4482b17da1
Mahdiyar <m.poursalari3200@gmail.com>
2024-05-13 17:49:55 +0330

    Question component was created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Box } from "@mui/material"
+import { Box } from "@mui/material";
+import Questions from "./components/Questions";
 
 const App = () => {
   return (
-    <Box bgcolor={"customeGreen.main"}>
-      
+    <Box
+      bgcolor={"customBlue.main"}
+      height={"98%"}
+      width={"99%"}
+      boxSizing={"border-box"}
+      position={"absolute"}
+      top={"50%"}
+      left={"50%"}
+      sx={{ transform: "translate(-50%, -50%)" }}
+    >
+      <Questions />
     </Box>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/utils/customThem.ts
+++ src/utils/customThem.ts
@@ -2,16 +2,22 @@ import { createTheme } from "@mui/material/styles";
 import { Theme } from "@mui/material/styles";
 
 type customPaletteOption = {
-  customeGreen?: {
+  customBlue: {
     main: string;
   };
+  customYellow: {
+    main: string
+  }
 } & Theme["palette"];
 
 
 export const myTheme = createTheme({
   palette: {
-    customeGreen: {
-      main: "#40A578",
+    customBlue: {
+      main: "#03AED2",
     },
+    customYellow: {
+      main: "#FDDE55"
+    }
   } as customPaletteOption,
 });
