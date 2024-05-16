76b52d1e9f5678eed7d84fbf6d54fb23b4a4c28f
Mahdiyar <m.poursalari3200@gmail.com>
2024-05-16 09:25:19 +0330

    Store provider was added.

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@mui/material";
 import { myTheme } from "./utils/customThem.ts";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={myTheme}>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={myTheme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
