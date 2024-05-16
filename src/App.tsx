6b8269292ca22914e14420698a354b09110bd660
Mahdiyar <m.poursalari3200@gmail.com>
2024-05-16 10:08:12 +0330

    All questions recives from server.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Box } from "@mui/material";
 import Questions from "./components/Question";
 import OptionsList from "./components/OptionsList";
-
+import { getQuestionsFromServer, selectAllQuestion } from "./store/questionSlice";
+import { useEffect } from "react";
+import { useQuestionSelector } from "./store/hooks";
+import { useQuestionDispatch } from "./store/hooks";
 const boxStyle = {
   bgcolor: "customBlue.main",
   height: "98%",
@@ -14,6 +17,14 @@ const boxStyle = {
 }
 
 const App = () => {
+  const dispatch = useQuestionDispatch()
+
+  useEffect(() => {
+    dispatch(getQuestionsFromServer())
+  }, [])
+
+  const questions = useQuestionSelector(selectAllQuestion);
+
   return (
     <Box {...boxStyle}>
       <Questions />
