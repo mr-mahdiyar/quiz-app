926b96b3ff216332efe44b2874f750b21daafaa5
Mahdiyar <m.poursalari3200@gmail.com>
2024-05-16 09:26:50 +0330

    CustomInput for addQuestionForm was created.

---
+++ src/components/CustomInput.tsx
@@ -0,0 +1,31 @@
+import { FormControl, Input } from "@mui/material";
+import { ChangeEvent, FormEvent } from "react";
+
+type CustomInputProps = {
+  placeHolder: string;
+  value: string;
+  onChangeValue: (newValue: string) => void;
+};
+
+const CustomInput = ({
+  placeHolder,
+  value,
+  onChangeValue,
+}: CustomInputProps) => {
+  return (
+    <>
+      <FormControl>
+        <Input
+          placeholder={placeHolder}
+          sx={{ mb: 5 }}
+          value={value}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            onChangeValue(event.target.value)
+          }}
+        />
+      </FormControl>
+    </>
+  );
+};
+
+export default CustomInput;
